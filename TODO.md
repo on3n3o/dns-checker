de5e42188b4cfe2705f6e5f9a203d8186aa4082f
Marcin Maciejewski <marcin.maciejewski@dataspace.pl>
2020-11-11 20:23:09 +0100

    [WIP] Working on development

---
+++ TODO.md
@@ -0,0 +1,10 @@
+# What's to do?
+
+- [x] Change command from host to dig `dig +noall +answer @1.1.1.1 sulan.pl dokdokodckdso.pl maciejews.ki A`
+- [x] Make array to check multiple websites in one request
+- [x] Give option to choose a ccTLD default .com
+- [x] Add time estimation on previous resoult, chunking system?
+- [ ] Give option to choose string length of domain default 2-3
+- [ ] Export to file .csv .xlsx
+- [ ] Write readme.md
+- [ ] Add Dockerfile and docker-compose.yml
--- app/Commands/CheckCommand.php
+++ app/Commands/CheckCommand.php
@@ -7,13 +7,13 @@ use LaravelZero\Framework\Commands\Command;
 
 class CheckCommand extends Command
 {
-    protected $validChars = ['a' , 'b', 'c', 'd', 'e' , 'f', 'g', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    protected $validChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
     /**
      * The signature of the command.
      *
      * @var string
      */
-    protected $signature = 'check {long=2} {ext=.pl}';
+    protected $signature = 'check {length=2} {ext=.pl}';
 
     /**
      * The description of the command.
@@ -29,22 +29,60 @@ class CheckCommand extends Command
      */
     public function handle()
     {
-    
-        foreach($this->validChars as $ch1){
-            $this->info('Checking ' . $ch1 . 'x.pl');
-            foreach($this->validChars as $ch2){
-                //foreach($this->validChars as $ch3){
-                    $output = null;
-                    exec('host -t a ' . $ch1 . $ch2 . /* $ch3  . */'.pl', $output);
-                    if(is_numeric(stripos($output[0], 'not found'))){
-                        $this->error($output[0]);
-                    }else{
-                        $this->line($output[0]);
-                    }
-                //}
+        $time = time();
+        $domains = collect();
+        /**
+         * Define domain array
+         */
+        foreach ($this->validChars as $validChar) {
+            $domains->put('a' . $validChar . $this->argument('ext') . '.', ['exists' => false]);
+        }
+        $this->line('Created domains array. Took: ' . time()-$time . ' seconds.');
+        $time = time();
+        /**
+         * Check domains via dig
+         */
+        exec('dig +noall +answer @1.1.1.1 ' . implode(' ', $domains->keys()->toArray()) . ' A', $outputArray);
+        foreach ($outputArray as $outputLine) {
+            $parts = preg_split('/\s+/', $outputLine);
+            $domains[$parts[0]] = [
+                'domain' => $parts[0],
+                'ttl' => $parts[1],
+                'record' => $parts[3],
+                'ip' => $parts[4],
+                'exists' => true
+            ];
+        }
+        $this->line('Checked domains information on dig. Took: ' . time() - $time . ' seconds.');
+        $time = time();
+        /**
+         * Check domains that has no information on dig via host
+         */
+        foreach ($domains->where('exists', false)->all() as $domainKey => $domainValue) {
+            $outputHost = null;
+            exec('host -t a ' . $domainKey, $outputHost);
+            if (is_numeric(stripos($outputHost[0], 'NXDOMAIN'))) {
+                $domains[$domainKey] = [
+                    'domain' => $domainKey,
+                    'exists' => false
+                ];
+            } else {
+                $domains[$domainKey] = [
+                    'domain' => $domainKey,
+                    'exists' => true
+                ];
             }
         }
-        $this->info('Finished!');
+
+        $this->line('Checked domains information on host. Took: ' . time()-$time . 'seconds.');
+        $time = time();
+        /**
+         * Print domains that are not registered
+         */
+        $count = 0;
+        $count += $domains->where('exists', false)->count();
+        $domainNames = $domains->where('exists', false)->keys()->toArray();
+        $this->info('Finished! Found: ' . $count . ' ' . implode(' ', $domainNames));
     }
 
     /**
